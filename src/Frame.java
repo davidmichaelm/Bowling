3fe7be6a73be4566a687125576a3deda6e6ad8c6
David Marquardt <davemarquardt@gmail.com>
2020-03-04 20:13:20 -0600

    Added properties to Frame

--- src/Frame.java
+++ src/Frame.java
@@ -1,2 +1,55 @@
+/*
+    Responsibilities:
+        Knows its first roll and second roll
+        Knows its own points
+        Knows if it’s a spare or a strike
+*/
+
 public class Frame {
+    public int firstRoll;
+    public int secondRoll;
+    public boolean spare;
+    public boolean strike;
+    public int points;
+
+
+    public int getFirstRoll() {
+        return firstRoll;
+    }
+
+    public void setFirstRoll(int firstRoll) {
+        this.firstRoll = firstRoll;
+    }
+
+    public int getSecondRoll() {
+        return secondRoll;
+    }
+
+    public void setSecondRoll(int secondRoll) {
+        this.secondRoll = secondRoll;
+    }
+
+    public boolean isSpare() {
+        return spare;
+    }
+
+    public void setSpare(boolean spare) {
+        this.spare = spare;
+    }
+
+    public boolean isStrike() {
+        return strike;
+    }
+
+    public void setStrike(boolean strike) {
+        this.strike = strike;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
 }
--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,9 @@
+/*
+    Responsibilities:
+        Add all frame points
+        Moves the action along
+*/
+
 public class Game {
+
 }
